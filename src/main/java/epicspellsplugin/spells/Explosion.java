3af8c45c9a8c5c268c8f71dd414f1195b375b7de
M0rica <afer2003.muelle@gmail.com>
2022-04-29 19:28:44 +0200

    change explosion strength

--- src/main/java/epicspellsplugin/spells/Explosion.java
+++ src/main/java/epicspellsplugin/spells/Explosion.java
@@ -4,14 +4,12 @@ import epicspellsplugin.BaseSpell;
 import epicspellsplugin.effects.RealisticExplosion;
 import epicspellsplugin.utils.DirectionalParticleCollection;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class Explosion extends BaseSpell {
@@ -36,6 +34,6 @@ public class Explosion extends BaseSpell {
         particles.add(new DirectionalParticleCollection(world, Particle.SMOKE_LARGE, location, velocity, 30, 0.3));
         particles.add(new DirectionalParticleCollection(world, Particle.SMOKE_NORMAL, location, velocity, 40, 0.3));
         particles.add(new DirectionalParticleCollection(world, Particle.CAMPFIRE_COSY_SMOKE, location, velocity, 40, 0.1));
-        new RealisticExplosion(world, location, 10, particles, 0.1, true);
+        new RealisticExplosion(world, location, 100, particles, 0.1, true);
     }
 }
