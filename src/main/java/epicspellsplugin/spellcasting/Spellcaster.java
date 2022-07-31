4c7de4b7fbd24742775d5f1c2a31d192237690b0
M0rica <afer2003.muelle@gmail.com>
2022-07-31 19:06:06 +0200

    project points onto 2d plane in real time for more accurate drawing, split tick() into smaller functions for better readability

--- src/main/java/epicspellsplugin/spellcasting/Spellcaster.java
+++ src/main/java/epicspellsplugin/spellcasting/Spellcaster.java
@@ -49,6 +49,8 @@ public class Spellcaster {
                         SpellcastPatternRecord spellcastPatternRecord = getSpellcastPatternRecord(mage);
                         List<Location> castingPoints = spellcastPatternRecord.getCastingPoints();
                         if(castingPoints.size() > 0) {
+                            Vector castingNormalPlane = spellcastPatternRecord.getNormalPlane();
+                            point = LocationUtils.projectPointOnPlane(castingPoints.get(0), castingNormalPlane, point);
                             Location lastPoint = castingPoints.get(castingPoints.size() - 1);
                             if (point.distance(lastPoint) > 0.1) {
                                 Location between = lastPoint.clone().add(point).multiply(0.5);
@@ -72,17 +74,12 @@ public class Spellcaster {
                     Vector castingNormalPlane = spellcastPatternRecord.getNormalPlane();
                     List<Location> transformedLocations = new ArrayList<>();
                     Location direction = player.getEyeLocation().add(player.getEyeLocation().getDirection().normalize().multiply(2));
-                    for(int i = 1; i < castingPoints.size(); i++){
-                        Location point = LocationUtils.projectPointOnPlane(castingPoints.get(0), castingNormalPlane, castingPoints.get(i));
-                        castingPoints.set(i, point);
-                    }
                     double angle = Math.acos(castingNormalPlane.dot(new Vector(0, 0, 1))/castingNormalPlane.length());
                     boolean mirror = false;
                     if(angle > Math.PI/2){
                         angle -= Math.PI;
                         mirror = true;
                     }
-                    System.out.println("Angle " + angle);
                     Vector axis = castingNormalPlane.clone().crossProduct(new Vector(0, 0, 1)).normalize();
                     for(int i = 1; i < castingPoints.size(); i++){
                         Vector temp = castingPoints.get(i).clone().toVector().subtract(castingPoints.get(0).toVector());
@@ -93,57 +90,10 @@ public class Spellcaster {
                         }
                         Location loc = direction.clone().add(temp);
                         transformedLocations.add(loc);
-                        //new DirectionalParticle(player.getWorld(), Particle.DRAGON_BREATH, loc, new Vector(), 0);
                     }
-                    int minSequenceSize = 4;
-                    List<int[]> filteredTempList = new ArrayList<>();
-                    List<int[]> filteredList = new ArrayList<>();
-                    System.out.println("============");
-                    for(int i = 1; i < transformedLocations.size(); i++){
-                        Vector temp = transformedLocations.get(i).clone().toVector().subtract(transformedLocations.get(i-1).clone().toVector()).normalize();
-                        List<Double> values = Arrays.asList(temp.getX(), temp.getY());
-                        int[] indexes = new int[2];
-                        for(int j = 0; j < indexes.length; j++){
-                            double value = values.get(j);
-                            double index;
-                            if(value > 0){
-                                index = Math.floor(value+0.7);
-                            } else {
-                                index = Math.ceil(value-0.7);
-                            }
-                            indexes[j] = (int) Math.round(index);
-                        }
-                        if(filteredTempList.isEmpty() || Arrays.equals(filteredTempList.get(0), indexes)){
-                            if(!filteredList.isEmpty() && Arrays.equals(filteredList.get(filteredList.size()-1), indexes)){
-                                continue;
-                            }
-                            filteredTempList.add(indexes);
-                        } else {
-                            if(filteredTempList.size() >= minSequenceSize){
-                                filteredList.add(filteredTempList.get(0));
-                            }
-                            filteredTempList.clear();
-                            filteredTempList.add(indexes);
-                        }
-                    }
-                    if(filteredTempList.size() >= minSequenceSize){
-                        filteredList.add(filteredTempList.get(0));
-                    }
-                    filteredList.forEach(array -> System.out.println(Arrays.toString(array)));
-                    List<int[]> lineTypes = Arrays.asList(
-                            new int[]{1, 1}, new int[]{1, 0}, new int[]{1, -1},
-                            new int[]{0, 1}, new int[]{0, 0}, new int[]{0, -1},
-                            new int[]{-1, 1}, new int[]{-1, 0}, new int[]{-1, -1}
-                    );
-                    List<Integer> lines = new ArrayList<>();
-                    for(int[] line: filteredList){
-                        for(int i = 0; i < lineTypes.size(); i++){
-                            if(Arrays.equals(lineTypes.get(i), line)){
-                                lines.add(i);
-                            }
-                        }
-                    }
-                    System.out.println(Arrays.toString(lines.toArray()));
+                    List<int[]> filteredList = pointsToFilteredVectors(transformedLocations, 4);
+                    List<Integer> lines = filteredVectorsToPattern(filteredList);
+                    log.info(String.format("Recorded pattern %s", Arrays.toString(lines.toArray())));
                     if(isSpellBinding(mage)){
                         completeSpellBinding(mage, lines);
                     } else {
@@ -160,6 +110,58 @@ public class Spellcaster {
         }
     }
 
+private List<int[]> pointsToFilteredVectors(List<Location> points, int minSequenceSize){
+    List<int[]> filteredTempList = new ArrayList<>();
+    List<int[]> filteredList = new ArrayList<>();
+    for(int i = 1; i < points.size(); i++){
+        Vector temp = points.get(i).clone().toVector().subtract(points.get(i-1).clone().toVector()).normalize();
+        List<Double> values = Arrays.asList(temp.getX(), temp.getY());
+        int[] indexes = new int[2];
+        for(int j = 0; j < indexes.length; j++){
+            double value = values.get(j);
+            double index;
+            if(value > 0){
+                index = Math.floor(value+0.7);
+            } else {
+                index = Math.ceil(value-0.7);
+            }
+            indexes[j] = (int) Math.round(index);
+        }
+        if(filteredTempList.isEmpty() || Arrays.equals(filteredTempList.get(0), indexes)){
+            if(!filteredList.isEmpty() && Arrays.equals(filteredList.get(filteredList.size()-1), indexes)){
+                continue;
+            }
+            filteredTempList.add(indexes);
+        } else {
+            if(filteredTempList.size() >= minSequenceSize){
+                filteredList.add(filteredTempList.get(0));
+            }
+            filteredTempList.clear();
+            filteredTempList.add(indexes);
+        }
+    }
+    if(filteredTempList.size() >= minSequenceSize){
+        filteredList.add(filteredTempList.get(0));
+    }
+    return filteredList;
+}
+
+private List<Integer> filteredVectorsToPattern(List<int[]> filteredList){
+    List<int[]> lineTypes = Arrays.asList(
+            new int[]{1, 1}, new int[]{1, 0}, new int[]{1, -1},
+            new int[]{0, 1}, new int[]{0, 0}, new int[]{0, -1},
+            new int[]{-1, 1}, new int[]{-1, 0}, new int[]{-1, -1}
+    );
+    List<Integer> lines = new ArrayList<>();
+    for(int[] line: filteredList){
+        for(int i = 0; i < lineTypes.size(); i++){
+            if(Arrays.equals(lineTypes.get(i), line)){
+                lines.add(i);
+            }
+        }
+    }
+    return lines;
+}
     public void setupSpellBinding(Mage mage, String spellName){
         Player player = mage.getPlayer();
         if(spellManager.getSpellNames().contains(spellName)){
